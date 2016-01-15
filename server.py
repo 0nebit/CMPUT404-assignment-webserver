ec6d37d37f6967b9fe6e5bb5ef94b79863e3852a
0nebit <chuan4@ualberta.ca>
2016-01-14 21:11:17 -0700

    progress

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import SocketServer
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -28,11 +29,56 @@ import SocketServer
 
 
 class MyWebServer(SocketServer.BaseRequestHandler):
-    
+
+    def __init__(self, request, client_address, server):
+        self.response_200 = "HTTP/1.1 200 OK\r\n"
+        self.response_404 = "HTTP/1.1 404 Not Found\r\n"
+        SocketServer.BaseRequestHandler.__init__(self, request,
+                                                 client_address, server)
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+        #print ("Got a request of: %s\n" % self.data)
+
+        formatted = self.data.split("\r\n")
+        self.print_request(formatted)
+
+        path = formatted[0].split(" ")[1]
+            
+        if (path == "/"):
+            self.request.sendall(self.response_200)
+            return
+
+        root_path = os.getcwd() + "/www"
+
+        if (not os.path.isfile(root_path+path)):
+            self.request.sendall(self.response_404)
+            return
+        #os.direxists("path")
+        elif (path == "/index.html"):
+            self.add_file(path)
+            self.request.sendall(self.response_200)
+        else:
+            self.request.sendall(self.response_200)
+        
+    def add_file(self, file_path):
+        root_path = os.getcwd() + "/www"
+        file0 = open(root_path+file_path, "r")
+
+        self.response_200 += "\n"
+        for line in file0:
+            line = line.strip("\n")
+            self.response_200 += line+"\r\n"
+        file0.close()
+        
+        return
+
+    def print_request(self, request_str):
+        print "New request:"
+        for line in request_str:
+            print line
+
+        return
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
